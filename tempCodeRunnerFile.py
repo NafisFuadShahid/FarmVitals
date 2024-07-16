156147ca446482b6caa0b7ac2b04cc2da5c5fda9
NafisFuadShahid <fuadnafis9191@gmail.com>
2024-07-17 04:25:01 +0600

    ....

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-train_generator.class_indices
+from tensorflow import keras
+from keras._tf_keras.keras.models import load_model
+from keras._tf_keras.keras.preprocessing import image
+from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
+
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import sys
