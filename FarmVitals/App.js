103f3c5fddd79fb876bea7555eefa96835ca66c1
shadabtanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2024-08-09 01:24:52 +0600

    package update again

--- FarmVitals/App.js
+++ FarmVitals/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontFamily: "Jost" }}>Welcome to FarmVitals</Text>
+      <Text style={{ fontFamily: "Jost Regular" }}>Welcome to FarmVitals</Text>
       <StatusBar style="auto" />
     </View>
   );
