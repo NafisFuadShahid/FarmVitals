10db03320c024bde4614079a90ddd1cfd116a0b6
shadabtanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2024-08-12 02:36:35 +0600

    Update App.js

--- FarmVitals/App.js
+++ FarmVitals/App.js
@@ -16,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000000",
+    backgroundColor: "#121212",
     alignItems: "center",
     justifyContent: "center",
   },
